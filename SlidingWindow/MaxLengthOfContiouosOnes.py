a22726337f2f85c569d3575dbd9a8e0bdea04384
T <shreyas.t@sap.com>
2020-02-04 15:14:31 +0530

    Longest SubSequence of Continuous Ones with K zeros replacable

---
+++ SlidingWindow/MaxLengthOfContiouosOnes.py
@@ -0,0 +1,31 @@
+def findIndexOfZero(arr):
+    n = len(arr)
+
+    start = 0
+    end = 0
+    count_zero = 0
+    index = 0
+    max_size = 1
+
+    for i in range(n):
+        end += 1
+
+        if arr[i] == 0:
+            count_zero += 1
+            prev_index_zero = i
+        
+        if count_zero >1:
+            while(arr[start] != 0):
+                start += 1
+            start+=1
+            count_zero -= 1
+        
+        if end - start > max_size:
+            max_size = end-start
+            index = prev_index_zero
+
+    print(arr[start:end])
+    return index
+
+arr = [0, 0, 1, 0, 1, 1, 1, 0, 1, 1 ]
+print(findIndexOfZero(arr))
---
+++ SlidingWindow/MaxSeqOfContinuousOnesK.py
@@ -0,0 +1,35 @@
+def longestSeq(arr, k):
+    n = len(arr)
+   
+    if k > n:
+        print("Longest sequence cannot be greater than the array itself")
+        return 
+    
+    start = 0
+    end = 0
+    max_start = 0
+    max_end = 0
+    count_zero = 0
+
+    for i in range(n):
+        end += 1
+            
+        if arr[i] == 0:
+            count_zero += 1
+
+        while count_zero > k:
+            while arr[start] != 0:
+                start += 1
+            start += 1
+            count_zero -= 1
+        
+        if end - start > max_end-max_start:
+            max_start = start
+            max_end = end
+    
+    return max_start, max_end-1
+
+arr = [1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0]
+print(longestSeq(arr, 2))
+
+ 
